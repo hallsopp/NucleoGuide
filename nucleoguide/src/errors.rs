d443a9b3eb1f181746acc51c654b8254631d5829
hallsopp <harryallsopp8@gmail.com>
2023-01-29 16:13:28 +0000

    barebones CLI, fmt + clippy optimisation

--- nucleoguide/src/errors.rs
+++ nucleoguide/src/errors.rs
@@ -1,23 +1,24 @@
-// custom module for errors 
-pub mod errors {
-    use std::fmt;
+// custom module for errors
+use std::fmt;
 
-    #[derive(Debug)]
-    pub enum RuntimeError {
-        IncorrectDNASequence,
-        NoGuidesFound,
-        InvalidPAM,
-        InvalidGRNAExclusionPattern,
-    }
-    
-    impl fmt::Display for RuntimeError {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match *self {
-                RuntimeError::IncorrectDNASequence => write!(f, "Incorrect DNA sequence provided."),
-                RuntimeError::NoGuidesFound => write!(f, "No compatible gRNAs found."),
-                RuntimeError::InvalidPAM => write!(f, "PAM Sequence is not valid."),
-                RuntimeError::InvalidGRNAExclusionPattern => write!(f, "gRNA exclusion pattern is not valid."),
+#[derive(Debug)]
+pub enum RuntimeError {
+    IncorrectDNASequence,
+    NoGuidesFound,
+    InvalidPAM,
+    InvalidGRNAExclusionPattern,
+}
+
+impl fmt::Display for RuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            RuntimeError::IncorrectDNASequence => write!(f, "Incorrect DNA sequence provided."),
+            RuntimeError::NoGuidesFound => write!(f, "No compatible gRNAs found."),
+            RuntimeError::InvalidPAM => write!(f, "PAM Sequence is not valid."),
+            RuntimeError::InvalidGRNAExclusionPattern => {
+                write!(f, "gRNA exclusion pattern is not valid.")
             }
         }
     }
 }
+
--- nucleoguide/src/lib.rs
+++ nucleoguide/src/lib.rs
@@ -1,15 +1,14 @@
-use std::collections::HashSet;
+use crate::errors::RuntimeError;
+use crate::grnas::run;
 use bio::alphabets;
-use crate::errors::errors::RuntimeError;
-use crate::grnas::grnas::run;
-mod grnas;
 mod errors;
+mod grnas;
 
 #[derive(Debug)]
 pub struct GuideDesign {
     seq: String,
     pam: String,
-    guide_features: Grna
+    guide_features: Grna,
 }
 
 #[derive(Debug)]
@@ -20,34 +19,41 @@ struct Grna {
 }
 
 impl GuideDesign {
-    // instantise a new guide design object 
-    pub fn new(s: String, p: String, gf_size: usize, gf_xc_pattern: String, gf_ic_pattern: String) -> Result<Self, RuntimeError> {
+    // instantise a new guide design object
+    pub fn new(
+        s: String,
+        p: String,
+        gf_size: usize,
+        gf_xc_pattern: String,
+        gf_ic_pattern: String,
+    ) -> Result<Self, RuntimeError> {
         let dna_ab = alphabets::dna::alphabet();
         // check that input seqeunce is valid DNA
-        if dna_ab.is_word(s.as_bytes()) == false {
+        if !dna_ab.is_word(s.as_bytes()) {
             Err(RuntimeError::IncorrectDNASequence)
         } else {
             Ok(GuideDesign {
                 seq: s,
                 pam: p,
-                guide_features: Grna { 
-                    size: gf_size, 
+                guide_features: Grna {
+                    size: gf_size,
                     xc_pattern: gf_xc_pattern,
                     ic_pattern: gf_ic_pattern,
-                }
+                },
             })
         }
     }
     // Funtion to search for grnas
     pub fn idgrnas(&self) -> Result<Vec<(&str, usize)>, RuntimeError> {
-        match run(&self.seq, &self.pam, &self.guide_features.size, 
-                &self.guide_features.xc_pattern, 
-                &self.guide_features.ic_pattern
-            ) {
+        match run(
+            &self.seq,
+            &self.pam,
+            &self.guide_features.size,
+            &self.guide_features.xc_pattern,
+            &self.guide_features.ic_pattern,
+        ) {
             Ok(n) => Ok(n),
-            Err(n) => Err(n)
-        } 
+            Err(n) => Err(n),
+        }
     }
 }
-
-
